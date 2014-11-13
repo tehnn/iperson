dbaccbc4d4e6358a2aac6eac05848befccecda2c
lenovo <lenovo@Lenovo-PC>
2014-11-13 10:45:39 +0700

    sdsdsd

--- src/acom/udon/iperson/AddActivity.java
+++ src/acom/udon/iperson/AddActivity.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import android.support.v7.app.ActionBarActivity;
 import android.content.Context;
+import android.database.sqlite.SQLiteCursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,70 +14,104 @@ import android.widget.EditText;
 
 public class AddActivity extends ActionBarActivity {
 
-	EditText txtName , txtAge;	
+	EditText txtName, txtAge;
 	boolean isNewPeople = false;
-	
-	Context context;	
+
+	Context context;
 	SQLiteDatabase db;
 	String name1 = "";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add);
-		txtName = (EditText)findViewById(R.id.editText1);
-		txtAge = (EditText)findViewById(R.id.editText2);
+		txtName = (EditText) findViewById(R.id.editText1);
+		txtAge = (EditText) findViewById(R.id.editText2);
 		context = getApplicationContext();
-		
 
-		Bundle extras = getIntent().getExtras();	
+		Bundle extras = getIntent().getExtras();
 
 		if (extras != null) {
 			name1 = extras.getString("name1");
-		}
-		
-		txtName.setText(name1);
-		
-		
-	}// end onCreate
-	
-	public void Save(View v){		
+			isNewPeople = true;
+			txtName.setText(name1);
 
-		
-		
-		if(!isNewPeople){			
-			/// insert
+			db = context.openOrCreateDatabase("db.db",
+					SQLiteDatabase.OPEN_READWRITE, null);
+			SQLiteCursor cur = (SQLiteCursor) db.rawQuery(
+					"select name,age from person where name='" + name1 + "'",
+					null);
+
+			cur.moveToFirst();
+			String t_age = cur.getString(1);
+
+			txtAge.setText(t_age);
+
+			cur.close();
+
+			db.close();
+		}
+
+	}// end onCreate
+
+	public void Save(View v) {
+
+		if (!isNewPeople) {
+			// / insert
 			String name = txtName.getText().toString();
 			String t_age = txtAge.getText().toString();
 			int age = Integer.parseInt(t_age);
-			
+
 			db = context.openOrCreateDatabase("db.db",
 					SQLiteDatabase.OPEN_READWRITE, null);
-			
+
 			db.setLocale(Locale.getDefault());
 			String SQL;
-			SQL = "INSERT INTO person (name, age) VALUES "
-					+ "('" + name + "'," + age + ")";
+			SQL = "INSERT INTO person (name, age) VALUES " + "('" + name + "',"
+					+ age + ")";
 
 			db.execSQL(SQL);
-			db.close();		
-			
-			
-		}else{
+			db.close();
+
+		} else {
 			// update
+
+			String txt_name = txtName.getText().toString();
+			String t_age = txtAge.getText().toString();
+			int age = Integer.parseInt(t_age);
+
+			db = context.openOrCreateDatabase("db.db",
+					SQLiteDatabase.OPEN_READWRITE, null);
+
+			db.setLocale(Locale.getDefault());
+			String SQL;
+			SQL = "update person set name='" + txt_name + "', age=" + age
+					+ " where name = '" + name1 + "'";
+
+			db.execSQL(SQL);
+			db.close();
+
 		}
-		
-		
+
 		finish();
 	}
-	
-	public void Del(View v){
-		
-		
+
+	public void Del(View v) {
+
+		db = context.openOrCreateDatabase("db.db",
+				SQLiteDatabase.OPEN_READWRITE, null);
+
+		db.setLocale(Locale.getDefault());
+		String SQL;
+		SQL = "delete from person where name = '" + name1 + "'";
+
+		db.execSQL(SQL);
+		db.close();
+
 		finish();
 	}
-	
-	public void Cancel(View v){
+
+	public void Cancel(View v) {
 		finish();
 	}
 
